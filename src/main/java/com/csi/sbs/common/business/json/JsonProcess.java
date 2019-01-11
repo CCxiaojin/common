1afbeaff7b0f6f4762f23db4733ae3488314c7eb
dapengchina <aoqishequ@163.com>
2019-01-11 16:14:26 +0800

    Change-Id: Ia78f20828a02e7c2ac7b455234ae532af6483edc

---
+++ src/main/java/com/csi/sbs/common/business/json/JsonProcess.java
@@ -0,0 +1,30 @@
+package com.csi.sbs.common.business.json;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+public class JsonProcess {
+	
+	   /**
+	    * 将返回的JSON字符串转为JSON对象(针对单个JSON字符串对象)
+	    */
+	   public static JSONObject changeToJSONObject(String str){
+		   return (JSONObject) JSON.parse(str);
+	   }
+	   
+	   /**
+	    * 将返回的JSON字符串转为JSONArray数组(针对JSON对象数组)
+	    */
+	   public static JSONArray changeToJSONArray(String str){
+		   return JSON.parseArray(str);
+	   }
+	   
+	   /**
+	    * 返回JSONObject的value
+	    */
+	   public static String returnValue(JSONObject jsonObject,String key){
+		   return jsonObject.getString(key);
+	   }
+
+}
